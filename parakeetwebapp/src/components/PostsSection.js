1595126232da73d462dd662a0845e702c4e45641
reblando8 <125155179+reblando8@users.noreply.github.com>
2024-11-19 22:52:04 -0500

    adding the navbar

---
+++ parakeetwebapp/src/components/PostsSection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function PostsSection() {
+    const posts = [
+        "/post1.jpg",
+        "/post2.jpg",
+        "/post3.jpg",
+    ];
+
+    return (
+        <div className="mt-4">
+            <h2 className="text-lg font-semibold mb-2">Posts</h2>
+            <div className="grid grid-cols-3 gap-4">
+                {posts.map((post, index) => (
+                    <img key={index} className="w-full h-64 object-cover rounded-lg" src={post} alt="Post" />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default PostsSection;
