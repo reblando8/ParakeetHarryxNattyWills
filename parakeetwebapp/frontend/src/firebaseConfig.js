e274ba203701a08c08156d08ca8a686eaa395512
reblando8 <125155179+reblando8@users.noreply.github.com>
2025-02-04 13:20:13 -0500

    doing stuff

---
+++ parakeetwebapp/frontend/src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD0ehr80W_ESCk6X_0cEJb0wcSOOW7b3io",
+  authDomain: "parakeetwebapp.firebaseapp.com",
+  projectId: "parakeetwebapp",
+  storageBucket: "parakeetwebapp.firebasestorage.app",
+  messagingSenderId: "429053294726",
+  appId: "1:429053294726:web:9c77eeab62a8e352093907",
+  measurementId: "G-5BTV8BP32Z"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
